d615154a2fd33bf330e13275e97bf4e7a3363a50
joaopaulo2148 <jpbpimenta06@gmail.com>
2024-09-08 17:40:29 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Atleta Brasileira Skatista Das Olimpíadas",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8",
+    },
+
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Atleta Brasileira Ginasta Das Olimpíadas",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro",
+    },
+    
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Atleta Brasileira Judoca Das Olimpíadas",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka",
+    }
+
+];
+
